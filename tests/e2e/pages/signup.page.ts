07a539c7c1edb30de4c3f76d4ed301b287544196
marcusadigun <adigunmarcus@gmail.com>
2026-04-23 15:12:19 -0400

    Add SignupPage e2e test class

---
+++ tests/e2e/pages/signup.page.ts
@@ -0,0 +1,34 @@
+import { type Page } from '@playwright/test';
+
+type SignupData = {
+    firstName: string;
+    surname: string;
+    level: string;
+    department: string;
+    phoneNumber: string;
+    email: string;
+    password: string;
+}
+
+export class SignupPage {
+    constructor(private readonly page: Page) {}
+
+    async goto() {
+        await this.page.goto('/auth/get-started')
+    }
+
+    async signup(data: SignupData) {
+        await this.page.getByRole('textbox', { name: 'First Name' }).fill(data.firstName);
+        await this.page.getByRole('textbox', { name: 'Surname' }).fill(data.surname);
+        await this.page.getByLabel('Level').selectOption(data.level);
+        await this.page.getByRole('textbox', { name: 'Enter your department' }).fill(data.department);
+        await this.page.getByPlaceholder('Enter your phone number').fill(data.phoneNumber);
+        await this.page.getByRole('textbox', { name: 'Email' }).fill(data.email);
+        await this.page.getByRole('textbox', { name: 'Password' }).fill(data.password);
+        await this.page.getByRole('button', { name: 'Sign Up' }).click();
+    }
+
+    getSuccessMessage() {
+        return this.page.getByText(/account created successfully/i)
+    }
+}
