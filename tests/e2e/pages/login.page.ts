81c258921075b7c73a91e93aeb9fb09c4e47b770
marcusadigun <adigunmarcus@gmail.com>
2026-04-23 12:57:28 -0400

    Refactor LoginPage e2e methods and replace `expectError` with `getErrorMessage`

--- tests/e2e/pages/login.page.ts
+++ tests/e2e/pages/login.page.ts
@@ -5,18 +5,16 @@ export class LoginPage {
     }
 
     async goto() {
-        // TODO: navigate to /auth/login
         await this.page.goto('/auth/login');
     }
 
     async login(_email: string, _password: string) {
-        // TODO: fill email/password and click login
         await this.page.getByRole('textbox', {name: 'Username'}).fill(username)
         await this.page.getByRole('textbox', {name: 'Password'}).fill(password)
         await this.page.getByRole('button', {name: 'Login'}).click()
     }
 
-    async expectError() {
-        await
+    getErrorMessage() {
+        return this.page.getByText('Invalid Credentials')
     }
 }
