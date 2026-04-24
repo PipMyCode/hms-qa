db9bb537234f4d846419d128dad8087d891e31cc
marcusadigun <adigunmarcus@gmail.com>
2026-04-24 06:19:50 -0400

    Refactor SignupPage: update attribute spacing and adjust constructor formatting

--- tests/e2e/pages/signup.page.ts
+++ tests/e2e/pages/signup.page.ts
@@ -1,4 +1,4 @@
-import { type Page } from '@playwright/test';
+import {type Page} from '@playwright/test';
 
 type SignupData = {
     firstName: string;
@@ -11,21 +11,22 @@ type SignupData = {
 }
 
 export class SignupPage {
-    constructor(private readonly page: Page) {}
+    constructor(private readonly page: Page) {
+    }
 
     async goto() {
         await this.page.goto('/auth/get-started')
     }
 
     async signup(data: SignupData) {
-        await this.page.getByRole('textbox', { name: 'First Name' }).fill(data.firstName);
-        await this.page.getByRole('textbox', { name: 'Surname' }).fill(data.surname);
+        await this.page.getByRole('textbox', {name: 'First Name'}).fill(data.firstName);
+        await this.page.getByRole('textbox', {name: 'Surname'}).fill(data.surname);
         await this.page.getByLabel('Level').selectOption(data.level);
-        await this.page.getByRole('textbox', { name: 'Enter your department' }).fill(data.department);
+        await this.page.getByRole('textbox', {name: 'Enter your department'}).fill(data.department);
         await this.page.getByPlaceholder('Enter your phone number').fill(data.phoneNumber);
-        await this.page.getByRole('textbox', { name: 'Email' }).fill(data.email);
-        await this.page.getByRole('textbox', { name: 'Password' }).fill(data.password);
-        await this.page.getByRole('button', { name: 'Sign Up' }).click();
+        await this.page.getByRole('textbox', {name: 'Email'}).fill(data.email);
+        await this.page.getByRole('textbox', {name: 'Password'}).fill(data.password);
+        await this.page.getByRole('button', {name: 'Sign Up'}).click();
     }
 
     getSuccessMessage() {
